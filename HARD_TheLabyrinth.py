13c8f465a720d97427e585011ca23f060ecac944
Thomas Weber <tweber36@gmail.com>
2019-12-17 21:10:26 +0100

    Defibrillators: remove unused lib

---
+++ HARD_TheLabyrinth.py
@@ -0,0 +1,24 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+# r: number of rows.
+# c: number of columns.
+# a: number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
+r, c, a = [int(i) for i in input().split()]
+map = {}
+
+while 1:
+    # kr: row where Kirk is located.
+    # kc: column where Kirk is located.
+    kr, kc = [int(i) for i in input().split()]
+    for i in range(r):
+        row = input()  # C of the characters in '#.TC?' (i.e. one line of the ASCII maze).
+        for j in range(len(list(row))):
+            map[(i, j)] = list(row)[j]
+
+
+    print(map, file=sys.stderr)
+    print("RIGHT")
